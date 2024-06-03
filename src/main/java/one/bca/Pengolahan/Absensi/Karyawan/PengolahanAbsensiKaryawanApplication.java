77a779937a8450590a47eddf113002a80bac1b7e
Paul Calvin <paulcalvin03@gmail.com>
2024-06-03 19:34:35 +0700

    first commit

---
+++ src/main/java/one/bca/Pengolahan/Absensi/Karyawan/PengolahanAbsensiKaryawanApplication.java
@@ -0,0 +1,13 @@
+package one.bca.Pengolahan.Absensi.Karyawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengolahanAbsensiKaryawanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PengolahanAbsensiKaryawanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/bca/Pengolahan/Absensi/Karyawan/PengolahanAbsensiKaryawanApplicationTests.java
@@ -0,0 +1,13 @@
+package one.bca.Pengolahan.Absensi.Karyawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengolahanAbsensiKaryawanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
